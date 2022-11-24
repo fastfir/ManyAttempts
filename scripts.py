83b1ea785969b92dd1a8eca219744c80c03daa87
fastfir <95661133+fastfir@users.noreply.github.com>
2022-11-23 22:33:19 -0800

    Added logic to what is tested

--- scripts.py
+++ scripts.py
@@ -1,5 +1,15 @@
 import os
+import nmap
+nm = nmap.PortScanner()
 def attempt(ip):
-    os.system("timeout 5 ftp " + ip)
-    os.system("anonymous")
-    
+    nm.scan(ip)
+    host = nm.all_hosts()
+    ports = nm[host]['tcp'].keys()
+    if ("21" in ports):
+        os.system("ftp " + ip)
+        os.system("anonymous")
+    if ("80" in ports):
+        #a lot of things
+        s = "" #placeholder
+    if ("445" in ports):
+        os.system("smbclient -L ")
